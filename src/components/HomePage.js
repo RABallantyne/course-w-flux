7b8dd27e22175d0a299527b4a17cf95cc810016a
Jerkbert <raballantyne@gmail.com>
2020-01-06 09:30:41 -0700

    first component

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Administration</h1>
+      <p>React, Flux, and React router for responsive web apps.</p>
+    </div>
+  );
+}
+
+export default HomePage;
