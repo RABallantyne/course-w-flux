3bf82c454c8adc167d890b84e93dd2ed5bbc224e
Jerkbert <raballantyne@gmail.com>
2020-01-07 13:59:44 -0700

    set up basic routes, added header

---
+++ src/components/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h2> About </h2>
+        <p>We are using React.</p>
+      </>
+    );
+  }
+}
+export default AboutPage;
